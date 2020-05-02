894bbaeae412efce6eef9f5780fbd56e5c530201
Paulo De Mello <paulo.demello@kagenova.com>
2020-05-02 23:27:23 +0100

    added simple script for teleporting the camera rig to an object, when object is colliding with a 'teleportable' object

---
+++ VR Jam 2020/Assets/_Application/Scripts/Teleport.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+[RequireComponent(typeof(Rigidbody))] 
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] private float fadeTime;
+    [SerializeField] private SteamVR_Action_Boolean teleportAction;
+    //TODO find out how to reference Player position / head position without assigning field.
+    [SerializeField] private Transform cameraRig;
+    [SerializeField] private Transform head;
+
+    private bool isTeleportable;
+    private bool isTeleporting;
+    void Update()
+    {
+        if (teleportAction.GetStateUp(SteamVR_Input_Sources.Any))
+            TeleportCameraRigToThis();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //TODO discuss if more logic is required for teleportable objects otherwise use tag instead.
+        if (collision.gameObject.GetComponent("Teleportable") != null)
+            isTeleportable = true;
+        else
+            isTeleportable = false;
+    }
+
+    //TODO better names?!
+    private void TeleportCameraRigToThis()
+    {
+        if (!isTeleportable || isTeleporting)
+            return;
+
+        Vector3 groundPosition = new Vector3(head.position.x, cameraRig.position.y, head.position.z);
+        Vector3 translateVector = transform.position - groundPosition;
+
+        StartCoroutine(MoveRig(cameraRig, translateVector));
+    }
+
+    private IEnumerator MoveRig(Transform cameraRig, Vector3 translation)
+    {
+        isTeleporting = true;
+
+        SteamVR_Fade.Start(Color.black, fadeTime, true);
+
+        yield return new WaitForSeconds(fadeTime);
+        cameraRig.position += translation;
+
+        SteamVR_Fade.Start(Color.clear, fadeTime, true);
+
+        isTeleporting = false;
+    }
+}
