c0fc0120ea43833f244ee688cbab0feaa57cf666
Martin Ender <m.buettner.d@gmail.com>
2020-05-02 17:43:42 +0200

    Try adding a script

---
+++ VR Jam 2020/Assets/_Application/Scripts/FlyToHand.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+namespace VRJam2020
+{
+	public class FlyToHand : MonoBehaviour
+	{
+		[SerializeField] private Transform leftHand;
+		[SerializeField] private Transform rightHand;
+
+		private SteamVR_Input_Sources leftHandSource;
+		private SteamVR_Input_Sources rightHandSource;
+
+		private void Awake()
+		{
+			leftHandSource = SteamVR_Input_Sources.LeftHand;
+			rightHandSource = SteamVR_Input_Sources.RightHand;
+		}
+
+	}
+}
