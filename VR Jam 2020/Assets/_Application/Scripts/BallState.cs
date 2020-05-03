14ffcb1eb0eed010fbc6b7bba569a55fd2b1aa86
Paulo De Mello <paulo.demello@kagenova.com>
2020-05-03 17:42:20 +0100

    added different colours for different controllableStates, need to confirm colour choices...

--- VR Jam 2020/Assets/_Application/Scripts/BallState.cs
+++ VR Jam 2020/Assets/_Application/Scripts/BallState.cs
@@ -5,14 +5,43 @@ namespace VRJam2020
     public class BallState : MonoBehaviour
     {
         [SerializeField] private ControllableState initialControllableState;
-
+        [SerializeField] private Color32 bounceColour;
+        [SerializeField] private Color32 teleportColour;
+        [SerializeField] private Color32 stickyColour;
         public ControllableState ControllableState { get; set; }
         public ElementalState ElementalState { get; set; } = ElementalState.None;
         public bool IsGlowing { get; set; } = false;
 
+        private Material ballMaterial;
+
         private void Awake()
         {
+            ballMaterial = GetComponentInChildren<Renderer>().material;
             ControllableState = initialControllableState;
+        }
+
+        private void Update()
+        {
+            UpdateBallColour();
+        }
+
+        private void UpdateBallColour()
+        {
+            switch (ControllableState)
+            {
+            case ControllableState.Bounce:
+                ballMaterial.color = bounceColour;
+                break;
+            case ControllableState.Teleport:
+                ballMaterial.color = teleportColour;
+                break;
+            case ControllableState.Sticky:
+                ballMaterial.color = stickyColour;
+                break;
+            default:
+                break;
+            }
+
         }
     }
 }
