b8a6e64a03915681d850ea772c4cd22e0988b8eb
Paulo De Mello <paulo.demello@kagenova.com>
2020-05-05 18:55:03 +0100

    got pop up test working roughly

---
+++ VR Jam 2020/Assets/_Application/Scripts/TextManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace VRJam2020
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class TextManager : MonoBehaviour
+    {
+        private TextMeshProUGUI popUpText = null;
+        private float timeLeft;
+        private void Awake()
+        {
+            popUpText = GetComponent<TextMeshProUGUI>();
+        }
+        public void SetPopUpText(string text)
+        {
+            popUpText.SetText(text);
+            timeLeft = 15;
+        }
+
+        private void Update()
+        {
+            timeLeft -= Time.deltaTime;
+
+            if (timeLeft < 0)
+                popUpText.SetText("");
+        }
+
+
+    }
+}
---
+++ VR Jam 2020/Assets/_Application/Scripts/TextTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace VRJam2020
+{
+    [RequireComponent(typeof(Collider))]
+    public class TextTrigger : MonoBehaviour
+    {
+        [SerializeField] string popUpText = null;
+        [SerializeField] TextManager textManager = null;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.transform.root.GetComponent<Player>())
+                textManager.SetPopUpText(popUpText);
+        }
+    }
+}
+
