dfe4caf0de875198b0393278bfc1eb3444008665
Martin Ender <m.buettner.d@gmail.com>
2020-05-06 17:53:04 +0200

    WIP: enemy logic

---
+++ VR Jam 2020/Assets/_Application/Scripts/AnimatorParameters.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace VRJam2020
+{
+	public static class AnimatorParameters
+	{
+		public static int IsMoving => Animator.StringToHash("Is Moving");
+		public static int IsAttacking => Animator.StringToHash("Is Attacking");
+	}
+}
---
+++ VR Jam 2020/Assets/_Application/Scripts/Enemy.cs
@@ -0,0 +1,107 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace VRJam2020
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Enemy : MonoBehaviour
+    {
+        protected enum EnemyState
+        {
+            Idle,
+            Following,
+            Attacking,
+        }
+
+        [SerializeField] private Animator animator = null;
+
+        [SerializeField] private float attackRange = 1.5f;
+        [SerializeField] private float navGoalRefreshInterval = 0.5f;
+
+        private Transform goal;
+        private NavMeshAgent navMeshAgent;
+
+        private EnemyState currentState;
+
+        private float lastNavGoalRefresh;
+
+        private void Awake()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            currentState = EnemyState.Idle;
+        }
+
+        private void Update()
+        {
+            switch (currentState)
+            {
+            case EnemyState.Idle:
+                break;
+            case EnemyState.Following:
+                UpdateWhileFollowing();
+                break;
+            case EnemyState.Attacking:
+                UpdateWhileAttacking();
+                break;
+            }
+        }
+
+        private void UpdateWhileFollowing()
+        {
+
+        }
+
+        private void UpdateWhileAttacking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DiscoverPlayer(Transform player)
+        {
+            goal = player;
+            DetermineNextState();
+        }
+
+        private void DetermineNextState()
+        {
+            if ((goal.position - transform.position).magnitude < attackRange)
+            {
+                BeginAttack();
+            }
+            else
+            {
+                BeginFollowing();
+            }
+        }
+
+        private void BeginFollowing()
+        {
+            currentState = EnemyState.Following;
+            animator.SetBool(AnimatorParameters.IsMoving, true);
+
+            RefreshNavGoal();
+        }
+
+        private void RefreshNavGoal()
+        {
+            navMeshAgent.destination = goal.position;
+            lastNavGoalRefresh = Time.time;
+        }
+
+        private void BeginAttack()
+        {
+            currentState = EnemyState.Attacking;
+            animator.SetTrigger(AnimatorParameters.IsAttacking);
+        }
+
+        private void Start()
+        {
+            DOTween.Sequence()
+                .AppendCallback(() => navMeshAgent.destination = goal.position)
+                .AppendInterval(0.5f)
+                .SetLoops(-1);
+        }
+    }
+}
