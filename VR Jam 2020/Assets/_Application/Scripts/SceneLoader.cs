236b8f1e1ffe82c463b4203ac05d0dd75cb0bd37
Martin Ender <m.buettner.d@gmail.com>
2020-05-07 15:44:01 +0200

    Set up view volumes and bake occlusion data

--- VR Jam 2020/Assets/_Application/Scripts/SceneLoader.cs
+++ VR Jam 2020/Assets/_Application/Scripts/SceneLoader.cs
@@ -6,6 +6,5 @@ public class SceneLoader : MonoBehaviour
     private void Awake()
     {
         SceneManager.LoadScene("LevelGeometry", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Kitchen", LoadSceneMode.Additive);
     }
 }
