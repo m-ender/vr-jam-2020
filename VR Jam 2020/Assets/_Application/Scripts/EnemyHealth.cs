dfed4deb861cf04b3bbdbe8cb5ff4f3014d519e9
Martin Ender <m.buettner.d@gmail.com>
2020-05-07 21:24:27 +0200

    Flash enemies when taking damage

--- VR Jam 2020/Assets/_Application/Scripts/EnemyHealth.cs
+++ VR Jam 2020/Assets/_Application/Scripts/EnemyHealth.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
 
 namespace VRJam2020
 {
     public class EnemyHealth : Health
     {
+        [SerializeField] private Renderer modelRenderer;
+
+        [SerializeField] private Color damageHighlightColor;
+        [SerializeField] private float damageHighlightTime;
+
         protected override void Die()
         {
             StartCoroutine(DieNextFrame());
@@ -14,6 +21,13 @@ namespace VRJam2020
             yield return null;
 
             Destroy(gameObject);
+        }
+
+        protected override void ShowDamageFeedback()
+        {
+            DOTween.Sequence()
+                .Append(modelRenderer.material.DOColor(damageHighlightColor, damageHighlightTime / 2))
+                .Append(modelRenderer.material.DOColor(Color.white, damageHighlightTime / 2));
         }
     }
 }
