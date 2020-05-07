8d944d58d265b1eba42c070f97dfd3a6535147d6
Martin Ender <m.buettner.d@gmail.com>
2020-05-07 23:24:26 +0200

    Improve UI system

---
+++ VR Jam 2020/Assets/_Application/Scripts/CoverViewport.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace VRJam2020
+{
+	[RequireComponent(typeof(RectTransform))]
+	public class CoverViewport : MonoBehaviour
+	{
+		[SerializeField] private Camera targetCamera;
+
+		private RectTransform rectTransform;
+
+		private void Awake()
+		{
+			rectTransform = GetComponent<RectTransform>();
+		}
+
+		private void Start()
+		{
+			float width = rectTransform.rect.width;
+
+			rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, width / targetCamera.aspect);
+
+			float distanceFromCamera = transform.localPosition.z;
+
+			float scale = 2.0f * distanceFromCamera * Mathf.Tan(Mathf.Deg2Rad * (targetCamera.fieldOfView * 0.5f)) / width;
+
+			transform.localScale = scale * Vector3.one;
+		}
+	}
+}
--- VR Jam 2020/Assets/_Application/Scripts/PlayerHealth.cs
+++ VR Jam 2020/Assets/_Application/Scripts/PlayerHealth.cs
@@ -1,5 +1,7 @@
 ﻿using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Valve.VR;
 
 namespace VRJam2020
@@ -9,17 +11,34 @@ namespace VRJam2020
         [SerializeField] private Color damageHighlightColor;
         [SerializeField] private float damageHighlightTime;
 
+        [SerializeField] private Image solidColorOverlay;
+        [SerializeField] private TextManager textManager;
+        [SerializeField] private float gameOverDuration;
+
         protected override void Die()
         {
-            Debug.Log("u ded. sad.");
+            // Turn screen black
+            solidColorOverlay.DOColor(Color.black, 1f);
+            // Display message
+            textManager.ShowText(PopUpType.Alert, "You died. Better luck next time!", gameOverDuration);
+            // Restart game (i.e. load main scene) after timer
+
+            GameObject playerRoot = transform.root.gameObject;
+
+            DOTween.Sequence()
+                .InsertCallback(gameOverDuration, () => {
+                    Destroy(playerRoot);
+                    SceneManager.LoadScene("MainScene");
+                });
+
+            Destroy(this);
         }
 
         protected override void ShowDamageFeedback()
         {
-            SteamVR_Fade.Start(damageHighlightColor, damageHighlightTime / 2);
-
             DOTween.Sequence()
-                .InsertCallback(damageHighlightTime / 2, () => SteamVR_Fade.Start(Color.clear, damageHighlightTime / 2));
+                .Append(solidColorOverlay.DOColor(damageHighlightColor, damageHighlightTime / 2))
+                .Append(solidColorOverlay.DOColor(Color.clear, damageHighlightTime / 2));
         }
     }
 }
