1852600d9ed64ab4346d517b51243f0eb659b2c8
Martin Ender <m.buettner.d@gmail.com>
2020-05-07 21:41:40 +0200

    Flash field of view when player takes damage

--- VR Jam 2020/Assets/_Application/Scripts/PlayerHealth.cs
+++ VR Jam 2020/Assets/_Application/Scripts/PlayerHealth.cs
@@ -1,12 +1,25 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
+using Valve.VR;
 
 namespace VRJam2020
 {
     public class PlayerHealth : Health
     {
+        [SerializeField] private Color damageHighlightColor;
+        [SerializeField] private float damageHighlightTime;
+
         protected override void Die()
         {
             Debug.Log("u ded. sad.");
+        }
+
+        protected override void ShowDamageFeedback()
+        {
+            SteamVR_Fade.Start(damageHighlightColor, damageHighlightTime / 2);
+
+            DOTween.Sequence()
+                .InsertCallback(damageHighlightTime / 2, () => SteamVR_Fade.Start(Color.clear, damageHighlightTime / 2));
         }
     }
 }
