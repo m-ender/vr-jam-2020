fd0639c11eb8fb202e5503b569c8419b7133bc21
Paulo De Mello <paulo.demello@kagenova.com>
2020-05-08 02:13:50 +0100

    added in prefabs from example unity effects and set up bitchin death particle effects for enemies!

--- VR Jam 2020/Assets/_Application/Scripts/EnemyHealth.cs
+++ VR Jam 2020/Assets/_Application/Scripts/EnemyHealth.cs
@@ -1,26 +1,52 @@
 ﻿using DG.Tweening;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace VRJam2020
 {
+    [RequireComponent(typeof(Enemy))]
+    [RequireComponent(typeof(NavMeshAgent))]
     public class EnemyHealth : Health
     {
         [SerializeField] private Renderer modelRenderer;
 
         [SerializeField] private Color damageHighlightColor;
         [SerializeField] private float damageHighlightTime;
+        [SerializeField] private float particleEffectTime;
+        [SerializeField] private Material deathMaterial;
+
+        private Enemy enemy;
+        private NavMeshAgent navMeshAgent;
+        private Animator animator;
+
+        private ParticleSystem ps;
+
+        private int shaderProperty;
 
         protected override void Die()
         {
-            StartCoroutine(DieNextFrame());
+            AnimateDeath();
         }
 
-        private IEnumerator DieNextFrame()
+        private void AnimateDeath()
         {
-            yield return null;
+            enemy = GetComponent<Enemy>(); navMeshAgent = GetComponent<NavMeshAgent>();
+            animator = GetComponentInChildren<Animator>();
+            enemy.enabled = false; navMeshAgent.enabled = false; animator.enabled = false;
+            modelRenderer.material = deathMaterial;
+            shaderProperty = Shader.PropertyToID("_cutoff");
+            ps = GetComponentInChildren<ParticleSystem>();
 
-            Destroy(gameObject);
+            var main = ps.main;
+            main.duration = particleEffectTime;
+
+            ps.Play();
+
+            Sequence s = DOTween.Sequence();
+
+            s.Append(modelRenderer.material.DOFloat(1, shaderProperty, particleEffectTime));
+            s.AppendCallback(() => Destroy(gameObject));
         }
 
         protected override void ShowDamageFeedback()
